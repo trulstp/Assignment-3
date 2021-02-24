452d92ce7ae7454f219f2a3571fe1b753c66f3b3
Henrik Granum <henriklg@stud.ntnu.no>
2021-02-24 13:11:51 +0100

    test successfull, fixed comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,14 +22,10 @@ function toggleMenu(x) {
   }
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// Fixing the screen height for mobile phones properly
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
 window.addEventListener('resize', () => {
-  // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
