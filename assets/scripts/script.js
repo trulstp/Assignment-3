9e73a134e52b66745d41fb163104f8c5a8a69fc6
Henrik Granum <henriklg@stud.ntnu.no>
2021-02-24 12:56:16 +0100

    mobile screen height test

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,3 +20,11 @@ function toggleMenu(x) {
         hiddenItem.style.display = "list-item";
     }
   }
+
+
+  // We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
